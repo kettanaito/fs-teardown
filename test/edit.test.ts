6e405477510798ddca25591437023adac0465bfc
Artem Zakharchenko <kettanaito@gmail.com>
2021-08-31 14:56:54 +0200

    fix(edit): throw error when given a directory

--- test/edit.test.ts
+++ test/edit.test.ts
@@ -12,6 +12,10 @@ beforeAll(async () => {
   await api.prepare()
 })
 
+afterEach(async () => {
+  await api.reset()
+})
+
 afterAll(async () => {
   await api.cleanup()
 })
@@ -30,7 +34,14 @@ it('edits a newly created file', async () => {
 })
 
 it('throws an exception when editing a non-existing file', async () => {
-  await expect(api.edit('non-existing.txt', 'no-op')).rejects.toThrowError(
+  await expect(api.edit('non-existing.txt', 'no-op')).rejects.toThrow(
     'Failed to edit the file at "non-existing.txt": file does not exist. Did you forget to call "create"?',
+  )
+})
+
+it('throws an exception when given a directory', async () => {
+  await api.create({ dir: null })
+  await expect(api.edit('dir', 'no-op')).rejects.toThrow(
+    'Failed to edit the file at "dir": given path is a directory.',
   )
 })
